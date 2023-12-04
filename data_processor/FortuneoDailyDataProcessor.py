de8c72426454f49c2ba95df6b2703723c9fb07ed
aandriano <contact@aandriano.fr>
2023-12-04 22:45:58 +0100

    Handle empty data to scrape

--- data_processor/FortuneoDailyDataProcessor.py
+++ data_processor/FortuneoDailyDataProcessor.py
@@ -5,7 +5,10 @@ class FortuneoDailyDataProcessor:
     def process_bank_data(banking_account):
         scraper = BankDataProcessorFactory.create_scraper(banking_account)
         data_to_export = scraper.scrap_account_daily_data()
-        transformer = BankDataProcessorFactory.create_transformer(banking_account)
-        transformed_data = transformer.transform_collection(data_to_export)
-        dao = BankDataProcessorFactory.create_dao(banking_account)
-        dao.insert_many(transformed_data)
+        if data_to_export is not None:
+            transformer = BankDataProcessorFactory.create_transformer(banking_account)
+            transformed_data = transformer.transform_collection(data_to_export)
+            dao = BankDataProcessorFactory.create_dao(banking_account)
+            dao.insert_many(transformed_data)                
+        else:
+            print("Nothing to import.")
